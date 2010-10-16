c7f3965bafb7321573ee61510e69b937d8594447
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2010-10-16 16:07:01 +0000

    Add callbackIn and callbackOut parameters

--- jquery-fadetoggle.js
+++ jquery-fadetoggle.js
@@ -4,11 +4,12 @@
  * @author Tomasz Jakub Rup
  */
 (function($){
-	$.fn.fadeToggle = function(speed) {
+	$.fn.fadeToggle = function(speed, callbackIn, callbackOut) {
 		return this.each(function() {
-			if($(this).css('display') == 'none') $(this).fadeIn(speed);
+			if($(this).css('display') == 'none') $(this).fadeIn(speed, callbackIn);
 			else $(this).fadeOut(speed, function() {
-				$(this).css('display','none')
+				$(this).css('display','none');
+				if(callbackOut != undefined) callbackOut();
 			});
 		});
 	}
